9ecdc22da9c9b53ea0c9b4219cf148d1476bb10b
Stefanos Chrs <stefanoschrs@hotmail.com>
2018-03-19 21:45:28 +0100

    new images

--- scripts/rename-all.sh
+++ scripts/rename-all.sh
@@ -7,6 +7,19 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 find ${DIR}/../public -type f | while read file; do
+  if [[ "$file" == *".gitkeep" ]];then
+    continue;
+  fi
+
+  ext=${file##*.}
+  mv "${file}" "$(dirname "${file}")/`uuid`.${ext}"
+done
+
+find ${DIR}/../public-sfw -type f | while read file; do
+  if [[ "$file" == *".gitkeep" ]];then
+    continue;
+  fi
+
   ext=${file##*.}
   mv "${file}" "$(dirname "${file}")/`uuid`.${ext}"
 done
